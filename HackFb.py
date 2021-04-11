390d55c1e9492f0bad05af1e4e58b248027ed553
aldiansyah329 <81497102+aldiansyah329@users.noreply.github.com>
2021-04-11 16:22:02 +0700

    Update HackFb.py

--- HackFb.py
+++ HackFb.py
@@ -6,6 +6,38 @@
 #SUBSCRIBE YOUTUBE : Aldiansyah [aldi]
 #FOLLOW INSTAGRAM : @aldiansyah329
 
+import os,sys,time
+
+def bersih():
+        os.system('clear')
+
+usr='SUBSCRIBE'
+pwd='CHANNEL ALDI'
+def login():
+    bersih()
+    print('\033[1;34m+\033[1;31m---------------------------------------------\033[1;34m')
+    print('\033[1;31mSUBSCRIBE CHANNEL \033[1;37mAldiansyah [aldi]')
+    print('\033[1;34m+\033[1;31m---------------------------------------------\033[1;34m')
+    a = raw_input("\033[1;31mUsername \033[1;37m:")
+    y = raw_input("\033[1;31mPassword \033[1;37m:")
+    if a==usr and y==pwd:
+       print("\033[1;34mLogin Berhasil")
+       time.sleep(1)
+       bersih()
+    elif a=="" and y=="":
+        print("\033[1;31mJangan Salah")
+        os.system("python2 FB.py")
+    else:
+        print("\033[1;37mPassword Salah")
+        os.system("python2 FB.py")
+
+
+
+
+
+
+login()
+
 #Import module
 import os,sys,time,datetime,random,hashlib,re,threading,json,getpass,urllib,cookielib
 from multiprocessing.pool import ThreadPool
